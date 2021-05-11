e872b7d308d52c3162efcce2e2495a0c9418c9e1
chz <zhangcaihua@weli.cn>
2021-05-11 15:27:57 +0800

    second commit

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,10 +1,12 @@
 public class Person {
     private String name;
     private String sex;
+    private String address;
 
-    public Person(String name, String sex) {
+    public Person(String name, String sex, String address) {
         this.name = name;
         this.sex = sex;
+        this.address = address;
     }
 
     @Override
